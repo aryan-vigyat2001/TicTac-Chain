00d555fd46c3c4c017c86d784f5ddec89f716c2f
Fidal <fidal15perfect@gmail.com>
2023-10-16 16:25:22 +0530

    tic tac toe added frontend

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,14 @@
 
 import { ModeToggle } from "@/components/Toggletheme";
 import { Button } from "@/components/ui/button";
+
+
+import { useSigner } from "@thirdweb-dev/react";
+import { CHAIN_ID_TO_NAME } from "@certusone/wormhole-sdk";
+// import { describe, expect, test } from "@jest/globals";
+import { ethers } from "ethers";
+import { getHelloWormhole, getDeliveryHash, sleep } from "../../../ts-scripts/utils"
+
 import {
   useAddress,
   useChain,
@@ -20,6 +28,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { toast } from "@/components/ui/use-toast";
 import { generate } from "random-words";
+import Square from "../../components/Square";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Dashboard() {
   const chain = useChain();
@@ -28,13 +38,6 @@ export default function Dashboard() {
   const isMismatch = useNetworkMismatch();
   const disconnect = useDisconnect();
   const [mounted, setMounted] = useState(false);
-  const [words, setWords] = useState("");
-  const [boxes, setBoxes] = useState([]);
-  const [wordToGuess, setWordToGuess] = useState<any>("");
-  const [currentGuess, setCurrentGuess] = useState("");
-  const [turn, setTurn] = useState(0);
-  const [winner, setWinner] = useState<String | null>(null);
-  const [ans, setAns] = useState<any>("");
 
   useEffect(() => {
     if (!address) {
@@ -53,47 +56,118 @@ export default function Dashboard() {
       description: "Wallet disconnected successfully",
     });
   };
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [turn, setTurn] = useState("x");
+  const [winner, setWinner] = useState(null);
 
-  const createBoxes = (word: any) => {
-    const boxArray = word.split("").map((letter: any) => ({
-      letter,
-      isRevealed: false,
-    }));
-    setBoxes(boxArray);
+  const checkEndTheGame = () => {
+    for (let square of squares) {
+      if (!square) return false;
+    }
+    return true;
   };
 
-  const startNewGame = () => {
-    const randomWord = generate();
-    setAns(randomWord);
-    // console.log(randomWord, "randomWord");
-    setWordToGuess(randomWord);
-    setCurrentGuess("");
-    setTurn(0);
-    setWinner(null);
-    createBoxes(randomWord);
+  const checkWinner = () => {
+    const combos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let combo of combos) {
+      const [a, b, c] = combo;
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   };
 
-  const handleGuess = () => {
-    const isCorrect = currentGuess === wordToGuess;
-    if (isCorrect) {
-      setWinner(turn);
-      setBoxes((prevBoxes: any) =>
-        prevBoxes.map((box: any) => ({ ...box, isRevealed: true }))
-      );
-    } else {
-      setBoxes((prevBoxes) =>
-        prevBoxes.map((box) => ({ ...box, isRevealed: false }))
-      );
-      setTurn(turn === 0 ? 1 : 0);
+  const updateSquares = (ind) => {
+    if (squares[ind] || winner) {
+      return;
+    }
+    const s = squares;
+    s[ind] = turn;
+    setSquares(s);
+    setTurn(turn === "x" ? "o" : "x");
+    const W = checkWinner();
+    if (W) {
+      setWinner(W);
+    } else if (checkEndTheGame()) {
+      setWinner("x | o");
     }
   };
+
+  const resetGame = () => {
+    setSquares(Array(9).fill(""));
+    setTurn("x");
+    setWinner(null);
+  };
+
+  const [sourceChain, setSourceChain] = useState<number>(6);
+  const [targetChain, setTargetChain] = useState<number>(5);
+
+
+  const signer = useSigner();
+  useEffect(() => {
+
+    if (chain?.chain === "polygon") {
+      setSourceChain(5);
+      setTargetChain(6);
+    }
+
+  }, [chain])
+
 
   // prevents hydration error
   useEffect(() => {
     setMounted(true);
-    startNewGame();
   }, []);
   if (!mounted) return null;
+
+
+  const runHelloWormholeIntegrationTest = async () => {
+
+    if (!signer) {
+      return;
+    }
+
+    const arbitraryGreeting = `TIC TAC TOE ${new Date().getTime()}`;
+    const sourceHelloWormholeContract = getHelloWormhole(sourceChain, signer);
+    const targetHelloWormholeContract = getHelloWormhole(targetChain, signer);
+
+    const cost = await sourceHelloWormholeContract.quoteCrossChainGreeting(targetChain);
+    console.log(`Cost of sending the greeting: ${ethers.utils.formatEther(cost)} testnet AVAX`);
+
+    console.log(`Sending greeting: ${arbitraryGreeting}`);
+    const tx = await sourceHelloWormholeContract.sendCrossChainGreeting(
+      targetChain,
+      targetHelloWormholeContract.address,
+      arbitraryGreeting,
+      { value: cost }
+    );
+    console.log(`Transaction hash: ${tx.hash}`);
+    const rx = await tx.wait();
+
+    // const sChain = sourceChain;
+    const deliveryHash = await getDeliveryHash(rx, CHAIN_ID_TO_NAME[sourceChain as keyof typeof CHAIN_ID_TO_NAME], { network: "TESTNET" });
+
+    console.log("Waiting for delivery...");
+
+    console.log(deliveryHash)
+    console.log(`Reading greeting`);
+  };
+
 
   return (
     <div className="h-screen w-screen flex justify-center items-center flex-col">
@@ -117,38 +191,90 @@ export default function Dashboard() {
           <ModeToggle />
         </div>
       </div>
-      <div>Ans: {ans}</div>
 
-      {winner !== null ? (
-        <div className="text-xl">
-          <p className="mb-2">
-            Player {winner + 1} wins! The word was: {wordToGuess}
-          </p>
-          <button
-            onClick={startNewGame}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-          >
-            Start New Game
-          </button>
+
+      <div className="tic-tac-toe">
+        <h1> TIC-TAC-TOE </h1>
+        <button onClick={() => resetGame()}>New Game</button>
+        <div className="game">
+          {Array.from("012345678").map((ind) => (
+            <Square
+              key={ind}
+              ind={ind}
+              updateSquares={updateSquares}
+              clsName={squares[ind]}
+            />
+          ))}
         </div>
-      ) : (
-        <div className="text-xl">
-          <p className="mb-2">Player {turn + 1}'s turn</p>
-          <input
-            type="text"
-            placeholder="Enter your guess"
-            value={currentGuess}
-            onChange={(e) => setCurrentGuess(e.target.value)}
-            className="rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <button
-            onClick={handleGuess}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ml-2"
-          >
-            Guess
-          </button>
+        <div className={`turn ${turn === "x" ? "left" : "right"}`}>
+          <Square clsName="x" />
+          <Square clsName="o" />
         </div>
-      )}
+        <AnimatePresence>
+          {winner && (
+            <motion.div
+              key={"parent-box"}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="winner"
+            >
+              <motion.div
+                key={"child-box"}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0, opacity: 0 }}
+                className="text"
+              >
+                <motion.h2
+                  initial={{ scale: 0, y: 100 }}
+                  animate={{
+                    scale: 1,
+                    y: 0,
+                    transition: {
+                      y: { delay: 0.7 },
+                      duration: 0.7,
+                    },
+                  }}
+                >
+                  {winner === "x | o" ? "No Winner :/" : "Win !! :)"}
+                </motion.h2>
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{
+                    scale: 1,
+                    transition: {
+                      delay: 1.3,
+                      duration: 0.2,
+                    },
+                  }}
+                  className="win"
+                >
+                  {winner === "x | o" ? (
+                    <>
+                      <Square clsName="x" />
+                      <Square clsName="o" />
+                    </>
+                  ) : (
+                    <>
+                      <Square clsName={winner} />
+                    </>
+                  )}
+                </motion.div>
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{
+                    scale: 1,
+                    transition: { delay: 1.5, duration: 0.3 },
+                  }}
+                >
+                  <button onClick={() => resetGame()}>New Game</button>
+                </motion.div>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </div>
   );
 }
