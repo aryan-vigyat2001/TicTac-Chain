55aee005b82df11e5aef45bd9fed0440c9edd74c
Jaydeep <jaydeep.dey03@gmail.com>
2023-10-22 09:29:23 +0530

    loading state

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,22 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {toast} from "@/components/ui/use-toast";
 import useDB from "@/hooks/useDB";
-import { useAddress } from "@thirdweb-dev/react";
-import { useTime } from "framer-motion";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import {useAddress} from "@thirdweb-dev/react";
+import {useTime} from "framer-motion";
+import {Loader2} from "lucide-react";
+import {useRouter} from "next/navigation";
+import {useState} from "react";
 
 export default function Dashboard() {
-  const { db } = useDB();
+  const {db} = useDB();
   const router = useRouter();
   const [gameNo, setGameNo] = useState(0);
   const address = useAddress();
 
+  const [loading1, setLoading1] = useState(false);
+  const [loading2, setLoading2] = useState(false);
 
   const createGamefunc = async () => {
     try {
@@ -33,33 +37,36 @@ export default function Dashboard() {
         .bind(newGameNo, address, new Date().toISOString())
         .run();
 
-      insertNewGame.then(({ meta: update }) => {
-        update.txn.wait()
-          .then(() => {
-            console.log("Success inserting data");
-            router.push(`/game/${newGameNo}`);
-          })
-          .catch(err => {
-            console.log("Error while waiting for the transaction:", err);
-          });
-      })
-        .catch(err => {
+      setLoading1(true);
+      insertNewGame
+        .then(({meta: update}) => {
+          update.txn
+            .wait()
+            .then(() => {
+              console.log("Success inserting data");
+              router.push(`/game/${newGameNo}`);
+            })
+            .catch((err) => {
+              console.log("Error while waiting for the transaction:", err);
+            });
+        })
+        .catch((err) => {
           console.log("Error inserting data:", err);
         });
 
       setTimeout(() => {
         router.push(`/game/${newGameNo}`);
+        setLoading1(false);
       }, 20000);
+
       // setGameNo(newGameNo);
       // setGameNo((prevGameNo) => {
       //   console.log("id of the state", prevGameNo);
 
       //   console.log("preparation done---")
 
-
       //   return prevGameNo; // Return the updated state
       // });
-
 
       // const insertNewGame = db
       //   .prepare(
@@ -79,8 +86,17 @@ export default function Dashboard() {
   const [gameId, setGameId] = useState(0);
 
   const handleSubmit = async () => {
-
-    console.log("handle submit")
+    console.log("handle submit");
+    setLoading2(true);
+    if (gameId === 0) {
+      toast({
+        title: "Game Id cannot be empty",
+        description: "Please enter a valid game id",
+        variant: "destructive",
+      });
+      setLoading2(false);
+      return;
+    }
     try {
       const stmt = db
         .prepare("SELECT * FROM tictactoegames_11155111_152 WHERE id = ?")
@@ -91,18 +107,26 @@ export default function Dashboard() {
       const count = result.results.length;
 
       if (count > 0) {
-        console.log(address, "address", address?.toLowerCase())
-        if (result.results[0].player1 && result.results[0].player1.toLowerCase() === address?.toLowerCase()) {
+        console.log(address, "address", address?.toLowerCase());
+        if (
+          result.results[0].player1 &&
+          result.results[0].player1.toLowerCase() === address?.toLowerCase()
+        ) {
           router.push(`/game/${gameId}`);
-        }
-        else if (result.results[0].player2 && result.results[0].player2.toLowerCase() === address?.toLowerCase()) {
+        } else if (
+          result.results[0].player2 &&
+          result.results[0].player2.toLowerCase() === address?.toLowerCase()
+        ) {
           router.push(`/game/${gameId}`);
-        }
-        else if (result.results[0].player2 && result.results[0].player1 && result.results[0].player2.toLowerCase() !== address?.toLowerCase() && result.results[0].player2.toLowerCase() !== "") {
+        } else if (
+          result.results[0].player2 &&
+          result.results[0].player1 &&
+          result.results[0].player2.toLowerCase() !== address?.toLowerCase() &&
+          result.results[0].player2.toLowerCase() !== ""
+        ) {
           alert("Game already full");
           return;
         }
-
       }
 
       if (count === 0) {
@@ -111,27 +135,38 @@ export default function Dashboard() {
       } else {
         // console.log("gameId", gameId);
         // console.log("address here", address);
-        const { meta: update } = await db
+        const {meta: update} = await db
           .prepare(
             `UPDATE tictactoegames_11155111_152 SET player2 = ? WHERE id = ?`
           )
           .bind(address, gameId)
           .run();
 
+        setLoading2(true);
         await update.txn.wait();
-        console.log("Success updating data")
+        console.log("Success updating data");
+        setLoading2(false);
         router.push(`/game/${gameId}`);
       }
     } catch (err: any) {
-      console.log("error occurred ----------")
+      console.log("error occurred ----------");
       console.log(err.message);
+    } finally {
+      setLoading2(false);
     }
   };
   return (
     <div className="grid place-items-center min-h-screen w-screen">
       <div className="flex flex-col space-y-5">
         <div>{address}</div>
-        <Button onClick={createGamefunc}>Create Game</Button>
+        {loading1 ? (
+          <Button disabled>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Creating..
+          </Button>
+        ) : (
+          <Button onClick={createGamefunc}>Create Game</Button>
+        )}
         <div className="flex space-x-3">
           <Input
             placeholder="Enter Game Id..."
@@ -140,7 +175,21 @@ export default function Dashboard() {
               setGameId(parseInt(e.target.value));
             }}
           />
-          <Button onClick={() => { handleSubmit(); handleSubmit() }}>Join Game</Button>
+          {!loading2 ? (
+            <Button
+              onClick={() => {
+                handleSubmit();
+                handleSubmit();
+              }}
+            >
+              Join Game
+            </Button>
+          ) : (
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Joining...
+            </Button>
+          )}
         </div>
       </div>
     </div>
